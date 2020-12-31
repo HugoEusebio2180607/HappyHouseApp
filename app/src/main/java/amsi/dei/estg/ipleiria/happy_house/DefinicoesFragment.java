c62f34cb0d275488d65cfaf2591bbedabe514894
David Simões <2170369@my.ipleiria.pt>
2020-12-31 17:15:34 +0000

    Definições (design)

--- app/src/main/java/amsi/dei/estg/ipleiria/happy_house/DefinicoesFragment.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/happy_house/DefinicoesFragment.java
@@ -7,8 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DefinicoesFragment extends Fragment {
+
+    private EditText editTextPassword;
+    private EditText editTextNewPassword;
+    private EditText editTextTelemovel;
+    private Button btnGuardar;
 
     public DefinicoesFragment() {
         // Required empty public constructor
@@ -18,6 +25,20 @@ public class DefinicoesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_definicoes, container, false);
+        View view = inflater.inflate(R.layout.fragment_definicoes, container, false);
+
+        editTextPassword = view.findViewById(R.id.etNewPassword);
+        editTextNewPassword = view.findViewById(R.id.etPassword);
+        editTextTelemovel = view.findViewById(R.id.ettelemovel);
+        btnGuardar = view.findViewById(R.id.btnGuardar);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        return view;
     }
 }
